1185233a1e202586a3ea9c2268a9c71caf979639
nori <contact@norilog.net>
2019-05-31 04:39:21 +0000

    takslist

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,22 +3,23 @@
 @section('content')
     @if (Auth::check())
         <div class="row">
-            <aside class="col-sm-4">
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="card-title">{{ Auth::user()->name }}</h3>
-                    </div>
-                    <div class="card-body">
-                        <img class="rounded img-fluid" src="{{ Gravatar::src(Auth::user()->email, 500) }}" alt="">
-                    </div>
-                </div>
-            </aside>
+            <!--<aside class="col-sm-4">-->
+            <!--    <div class="card">-->
+            <!--        <div class="card-header">-->
+            <!--            <h3 class="card-title">{{ Auth::user()->name }}</h3>-->
+            <!--        </div>-->
+            <!--        <div class="card-body">-->
+            <!--            <img class="rounded img-fluid" src="{{ Gravatar::src(Auth::user()->email, 500) }}" alt="">-->
+            <!--        </div>-->
+            <!--    </div>-->
+            <!--</aside>-->
             <div class="col-sm-8">
                 @if (count($tasks) > 0)
                     @include('tasks.index', ['tasks' => $tasks])
                 @endif
             </div>
         </div>
+         {!! link_to_route('tasks.create', '新規タスクの投稿', null, ['class' => 'btn btn-primary']) !!}
     @else
         <div class="center jumbotron">
             <div class="text-center">
